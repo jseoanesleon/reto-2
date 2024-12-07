15f3932016a6e174b387964ba17384af53b6ec35
Jairo Francisco Seoanes Leon <jafrasele@gmail.com>
2024-12-06 21:26:59 -0500

    add contador visita y carrito compra

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -51,9 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
         ,
         {
             id: 8,
-            nombre: 'Licor Coreano',
-            precio: 70000,
-            imagen: 'assets/img/soju8.jpg'
+            nombre: '',
+            precio: 0,
+            imagen: ''
         },
         {
             id: 9,
@@ -89,37 +89,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderizarProductos() {
         DOMitems.innerHTML = "";
-
         const filtro = filtroSelect.value;
         const productosFiltrados = baseDeDatos.filter(producto => 
             filtro === "todas" || producto.categoria === filtro
         );
-
         productosFiltrados.forEach((info) => {
             const miNodo = document.createElement('div');
             miNodo.classList.add('card', 'col-sm-4');
-            
             const miNodoCardBody = document.createElement('div');
             miNodoCardBody.classList.add('card-body');
-            
             const miNodoTitle = document.createElement('h6');
             miNodoTitle.classList.add('card-title');
             miNodoTitle.textContent = info.nombre;
-            
             const miNodoImagen = document.createElement('img');
             miNodoImagen.classList.add('img-fluid');
             miNodoImagen.setAttribute('src', info.imagen);
-            
             const miNodoPrecio = document.createElement('p');
             miNodoPrecio.classList.add('card-text');
             miNodoPrecio.textContent = `${info.precio}${divisa}`;
-            
             const miNodoBoton = document.createElement('button');
             miNodoBoton.classList.add('btn', 'btn-primary');
             miNodoBoton.textContent = 'Agregar';
             miNodoBoton.setAttribute('marcador', info.id);
             miNodoBoton.addEventListener('click', anadirProductoAlCarrito);
-
             miNodoCardBody.appendChild(miNodoImagen);
             miNodoCardBody.appendChild(miNodoTitle);
             miNodoCardBody.appendChild(miNodoPrecio);
@@ -128,66 +120,53 @@ document.addEventListener('DOMContentLoaded', () => {
             DOMitems.appendChild(miNodo);
         });
     }
-
 // Obtén el contador del almacenamiento local
-let visitas = localStorage.getItem('contadorVisitas');
-
+    let visitas = localStorage.getItem('contadorVisitas');
 // Si no hay visitas almacenadas, inicializa a 0
-if (!visitas) {
-    visitas = 0;
-}
-
+    if (!visitas) {
+        visitas = 0;
+    }
 // Incrementa el contador
-visitas++;
-
+    visitas++;
 // Guarda el nuevo contador en el almacenamiento local
-localStorage.setItem('contadorVisitas', visitas);
-
+    localStorage.setItem('contadorVisitas', visitas);
 // Muestra el contador en la página
-document.getElementById('contador').textContent = visitas;
-
+    document.getElementById('contador').textContent = visitas;
     function anadirProductoAlCarrito(evento) {
         carrito.push(evento.target.getAttribute('marcador'));
         renderizarCarrito();
         guardarCarritoEnLocalStorage();
         handleCarritoValue(carrito.length);
     }
-
     function handleCarritoValue(value) {
         const carritoContainer = document.getElementById("carrito-value");
         carritoContainer.textContent = `${value}`;
     }
-
     function renderizarCarrito() {
         DOMcarrito.textContent = '';
         const carritoSinDuplicados = [...new Set(carrito)];
-
         carritoSinDuplicados.forEach((item) => {
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
-
             const numeroUnidadesItem = carrito.reduce((total, itemId) => {
                 return itemId === item ? total += 1 : total;
             }, 0);
-
             const miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
             miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-            
             const miBoton = document.createElement('button');
             miBoton.classList.add('btn', 'btn-danger', 'mx-5');
             miBoton.textContent = 'X';
             miBoton.style.marginLeft = '1rem';
             miBoton.dataset.item = item;
             miBoton.addEventListener('click', borrarItemCarrito);
-            
             miNodo.appendChild(miBoton);
             DOMcarrito.appendChild(miNodo);
         });
         DOMtotal.textContent = calcularTotal();
     }
-
+    //borra carrito
     function borrarItemCarrito(evento) {
         const id = evento.target.dataset.item;
         carrito = carrito.filter((carritoId) => {
@@ -197,7 +176,7 @@ document.getElementById('contador').textContent = visitas;
         guardarCarritoEnLocalStorage();
         handleCarritoValue(carrito.length);
     }
-
+    //calcular el total
     function calcularTotal() {
         return carrito.reduce((total, item) => {
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
@@ -206,28 +185,26 @@ document.getElementById('contador').textContent = visitas;
             return total + miItem[0].precio;
         }, 0).toFixed(2);
     }
-
+    //vaciar todos los elementos del carrito
     function vaciarCarrito() {
         carrito = [];
         renderizarCarrito();
         localStorage.clear();
     }
-
+    //guardar en local el carrito
     function guardarCarritoEnLocalStorage() {
         miLocalStorage.setItem('carrito', JSON.stringify(carrito));
     }
-
+    //cargar del local el carriro
     function cargarCarritoDeLocalStorage() {
         if (miLocalStorage.getItem('carrito') !== null) {
             carrito = JSON.parse(miLocalStorage.getItem('carrito'));
             handleCarritoValue(carrito.length);
         }
     }
-
     // Eventos
     DOMbotonVaciar.addEventListener('click', vaciarCarrito);
     filtroSelect.addEventListener('change', renderizarProductos);
-
     // Inicio
     cargarCarritoDeLocalStorage();
     renderizarProductos();
